fc0e0484ba44420aeb995ea62a69f01d8e90a365
yoga-viss <kandukuriyoga@gmail.com>
2020-06-22 18:13:34 +0530

    first commit

---
+++ k.js
@@ -0,0 +1,15 @@
+ window.open = function() {};
+            window.print = function() {};
+            // Support hover state for mobile.
+            if (false) {
+                window.ontouchstart = function() {};
+            }
+
+
+
+
+
+
+
+
+           
